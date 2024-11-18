7eaa53190d8dd356c8bbf874be51768483d99966
Busdan_Joshua <revildobusdan@gmail.com>
2024-11-18 20:42:51 +0700

    adding folder

--- App.tsx
+++ App.tsx
@@ -2,11 +2,48 @@ import React from 'react';
 import SplashScreen from './src/page/SplashScreen';
 import SignIn from './src/page/SignIn';
 import SignUp from './src/page/SignUp';
-import AddTransaction from './src/page/AddTransaction';
 import Home from './src/page/Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AddTransaction from './src/page/AddTransaction';
+import FlashMessage from 'react-native-flash-message';
 import './src/config/Firebase';
 
+const Stack = createNativeStackNavigator();
+
 const App = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="AddTransaction"
+          component={AddTransaction}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+      <FlashMessage position="top" />
+    </NavigationContainer>
+  );
 };
+
 export default App;
