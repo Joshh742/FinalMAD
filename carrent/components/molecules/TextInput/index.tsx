2d3e29ce5e9ce882608afd6f68955843b0bc0393
AndreasResa <jrresa11@gmail.com>
2024-11-23 21:54:54 +0800

    add

--- carrent/components/molecules/TextInput/index.tsx
+++ carrent/components/molecules/TextInput/index.tsx
@@ -1,11 +1,16 @@
 import {StyleSheet, Text, View, TextInput as Input} from 'react-native';
 import React from 'react';
 
-const TextInput = ({label, placeholder}) => {
+const TextInput = ({label, placeholder, onChangeText, ...rest}) => {
   return (
     <View>
       <Text style={styles.label}>{label}</Text>
-      <Input style={styles.input} placeholder={placeholder} />
+      <Input
+        style={styles.input}
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+        {...rest}
+      />
     </View>
   );
 };
@@ -14,20 +19,16 @@ export default TextInput;
 
 const styles = StyleSheet.create({
   label: {
-    fontFamily: 'Poppins-Regular',
     fontSize: 16,
     color: '#020202',
+    paddingHorizontal: 55,
   },
   input: {
-    backgroundColor: '#FFFFFF', // Warna putih untuk kotak input
     borderWidth: 1,
-    borderColor: '#E5E5E5',
-    borderRadius: 8,
-    padding: 10,
-    shadowColor: '#000', // Warna bayangan
-    shadowOffset: {width: 0, height: 2}, // Posisi bayangan
-    shadowOpacity: 0.1, // Transparansi bayangan
-    shadowRadius: 4, // Ukuran radius bayangan
-    elevation: 3, // Bayangan untuk perangkat Android
+    borderColor: '#020202',
+    borderRadius: 15,
+    padding: 15,
+    color: 'black',
+    marginHorizontal: 50,
   },
 });
