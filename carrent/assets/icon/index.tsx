2b35abc16e7b371bf9ed6a766256df079e66deab
AndreasResa <jrresa11@gmail.com>
2024-11-23 21:14:39 +0800

    add

--- carrent/assets/icon/index.tsx
+++ carrent/assets/icon/index.tsx
@@ -1,4 +1,7 @@
 import LogoCarrent from './logo-carrent.svg';
 import NullPhoto from './nullphoto.svg';
+import Arrowback from './Arrowback.svg';
+import Fortuner from './Fortuner.png';
+import Brio from './honda-brio.jpg';
 
-export {LogoCarrent, NullPhoto};
+export {LogoCarrent, NullPhoto, Arrowback, Fortuner, Brio};
